8827b92c8e4e28f4b89ae4c6a433ca6650200aba
Lennart Koehnke <lennartkoehnke@outlook.de>
2023-01-16 12:13:20 +0100

    just added a discription

--- obejectOrienatedApproach/Chessbot.m
+++ obejectOrienatedApproach/Chessbot.m
@@ -11,5 +11,18 @@ classdef Chessbot
   
         end
 
+        function [robot_move, beat] = calculateMove(player_move)
+            % ..
+            % 
+            %
+            % === INPUT ARGUMENTS ===
+            % player_move : move made by the player
+            %
+            % === OUTPUT ARGUMENTS ===
+            % robot_move : move the robot will be executing next
+            % beat : bool, if there is a chesspiece at the goal field 
+
+        end
+
     end
 end
