3f1ec0429ff73749cdb5ba02c3167adeff206b98
Lennart Koehnke <lennartkoehnke@outlook.de>
2023-01-22 17:28:18 +0100

    added some comments

--- obejectOrienatedApproach/Robot.m
+++ obejectOrienatedApproach/Robot.m
@@ -4,18 +4,23 @@ classdef Robot
     % Funktionen, die dieser auführen kann.
 
     properties
+        % hier werden alle nötigen Attribute des Roboters deklariert
+
         group
     end
 
     methods (Access = public)
         function obj = Robot()
             %Construct an instance of this class
+            % hier werden alle  Attribute des Roboters initializiert
 
         end
 
-        function success = makeMove(startPos, endPos)
+        function makeMove(obj, robot_Move)
+            % === INPUT ARGUMENTS ===
+            % robot_Move : sting, move the robot has to executre (eg.: 'd2d4')
+
             %TODO
-            success = 1;
         end
     end
 
