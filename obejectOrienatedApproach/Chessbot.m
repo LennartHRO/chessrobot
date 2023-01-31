2ef4f2a316b96067eb4300fe5939ffb1fe14a1d3
quân <116118843+lagessiehcs@users.noreply.github.com>
2023-01-31 13:11:36 +0100

    new setup function in chessbot

--- obejectOrienatedApproach/Chessbot.m
+++ obejectOrienatedApproach/Chessbot.m
@@ -4,8 +4,23 @@ classdef Chessbot
 
     end
 
-    methods
+    methods (Static)
         function obj = Chessbot()
+
+        end
+        
+        
+        function setup()
+
+            %Open in.txt to write and close again (to delete any old data).
+            clearfile = fopen('C:\Users\lenna\OneDrive - TUM\Uni\Entickelung modularer Roboter\Schachroboter\IO\in.txt','w'); 
+            fclose(clearfile);
+
+            %Open in.txt again to read
+            Input = fopen('C:\Users\lenna\OneDrive - TUM\Uni\Entickelung modularer Roboter\Schachroboter\IO\in.txt','r'); 
+                    
+            %Open out.txt to write (append)
+            Output = fopen('C:\Users\lenna\OneDrive - TUM\Uni\Entickelung modularer Roboter\Schachroboter\IO\out.txt','a');
 
         end
 
@@ -21,7 +36,7 @@ classdef Chessbot
             % beat : bool, if there is a chesspiece at the goal field
 
             
-            %Open file out.txt (this line goes to case 0 for setting up)
+            %Open file out.txt (this line goes to function setup for setting up)
             %Output = fopen('C:\Users\lenna\OneDrive - TUM\Uni\Entickelung modularer Roboter\Schachroboter\IO\out.txt','a'); %TODO: open file from path f = fopen('../../filename');
             
             
@@ -29,7 +44,7 @@ classdef Chessbot
             fprintf(Output,'%4s',player_move);    % Send Move to out.txt
 
             
-            %These 3 lines for setting up go in "case 0"
+            %These 3 lines for setting up go in setup
             %clearfile = fopen('C:\Users\lenna\OneDrive - TUM\Uni\Entickelung modularer Roboter\Schachroboter\IO\in.txt','w'); %Just open and then close in.txt again to clear old data
             %fclose(clearfile); 
             %Input = fopen('C:\Users\lenna\OneDrive - TUM\Uni\Entickelung modularer Roboter\Schachroboter\IO\in.txt','r'); %Now open file to read, TODO: path to in.txt
