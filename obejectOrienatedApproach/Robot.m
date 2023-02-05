fc28ec3390e127059889d98d50f770d6ff01472d
Sebastian <skuhn@hubertkuhn.de>
2023-02-05 20:39:14 +0100

    Tested state machine but found a bug in the Robot class that I can't fix right now.

--- obejectOrienatedApproach/Robot.m
+++ obejectOrienatedApproach/Robot.m
@@ -35,11 +35,13 @@ classdef Robot
     end
 
     methods(Access = public)
-        function obj = Robot()
+        function obj = Robot() % Works
+
             %Construct an instance of this class
             % hier werden alle  Attribute des Roboters initializiert
             %
             % Hebi-spezifisch:
+            %{
             obj.gripper_group = HebiLookup.newGroupFromNames('Greifer',{'Greifer'}); 
             obj.arm2R_group = HebiLookup.newGroupFromNames('Arm',{'Hinteres_Gelenk' ,'Vorderes_Gelenk'});
             obj.hinge_group = HebiLookup.newGroupFromNames('Hebende',{'Hebendes_Gelenk'});
@@ -50,6 +52,7 @@ classdef Robot
             obj.hinge_kin = HebiUtils.loadHRDF('2R_kinematics.xml'); % TODO: Create 1-dimensional hinge xml and add it
             obj.arm2R_trajGen = HebiTrajectoryGenerator(obj.arm2R_kin);
             obj.hinge_trajGen = HebiTrajectoryGenerator(obj.hinge_kin);
+            %}
             % Konstanten:
             obj.board_offset = [0.19 -0.133]; 
             obj.fig_gripped_min_torque = 0.3;
@@ -63,7 +66,7 @@ classdef Robot
             obj.xy_target  = [0 0]; % 2x1 vector of the desired arm position in the cartesian system
             obj.first_xy = [0 0];
             obj.second_xy = [0 0];
-            obj.arm_target_reached = false;
+            obj.arm_target_reached = 0;
             % State-Machine:
             % Initialization of the state machine and its constants. 
             % TODO: adjust the values of the constants
@@ -72,20 +75,13 @@ classdef Robot
 
         end
 
-        function [first_xy, second_xy] = field_to_robot_coords(obj, desired_move_mtx)
-            cell_size = 0.05; % [m] Breite = Höhe
-            first_pos_field_sys = cell_size*desired_move_mtx(1,:) - 0.5*[cell_size,cell_size];
-            second_pos_field_sys = cell_size*desired_move_mtx(2,:) - 0.5*[cell_size,cell_size];
-            first_xy = first_pos_field_sys + obj.board_offset;
-            second_xy = second_pos_field_sys + obj.board_offset;
-        end
-
-        function makeMove(obj, desired_move_mtx,figure_must_be_beat)
+        function makeMove(obj, desired_move_mtx,figure_must_be_beat) % Not yet done
             chess_move_done = false;
-            [first_xy, second_xy] = field_to_robot_coords(desired_move_mtx);
+            [first_xy, second_xy] = obj.field_to_robot_coords(desired_move_mtx);
             while chess_move_done == false
                 % The SM is run with only the the variables that are determined in Matlab being explicitly set:
-                step(obj.Arm_SM,statemachine_first_xy = first_xy, statemachine_second_xy = second_xy, arm_target_reached = obj.arm_target_reached); 
+                disp(obj.arm_target_reached);
+                step(obj.Arm_SM, statemachine_first_xy = first_xy, statemachine_second_xy = second_xy, statemachine_arm_target_reached = obj.arm_target_reached); 
                 switch obj.Arm_SM.function_to_run
                     case "none"
                         obj.arm_stop();
@@ -93,22 +89,35 @@ classdef Robot
                         obj.gripper_open();
                     case "gripper_close"
                         obj.gripper_close();
-                    case "arm_move"
-                        hinge_target = obj.Arm_SM.hinge_target;
+                    case "arm2R_move"
+                        obj.arm_target_reached = 0;
                         xy_target = obj.Arm_SM.xy_target;
-                        obj.arm_move(hinge_target,xy_target);
+                        obj.arm2R_move(xy_target);
+                    case "hinge_move"
+                        obj.arm_target_reached = 0;
+                        hinge_target = obj.Arm_SM.hinge_target;
+                        obj.hinge_move(hinge_target);
                     case "arm_stop"
                         obj.arm_stop();
                     otherwise
                         disp("Error");
                 end
                 chess_move_done = obj.Arm_SM.chess_move_done;
+                pause(1);
             end
         end
 
-        function arm2R_move(obj, xy_target)
-            % set the Hebigroup of arm2R Arm
+        function [first_xy, second_xy] = field_to_robot_coords(obj, desired_move_mtx) % Works
+            cell_size = 0.05; % [m] Breite = Höhe
+            first_pos_field_sys = cell_size*desired_move_mtx(1,:) - 0.5*[cell_size,cell_size];
+            second_pos_field_sys = cell_size*desired_move_mtx(2,:) - 0.5*[cell_size,cell_size];
+            first_xy = first_pos_field_sys + obj.board_offset;
+            second_xy = second_pos_field_sys + obj.board_offset;
+        end
 
+        function arm2R_move(obj, xy_target) % Works but not so accurate
+            disp("Moving 2R arm");
+            %{
             % generate trajectory
             obj.arm2R_trajGen.setAlgorithm('MinJerkPhase'); % MinJerk trajectories
             obj.arm2R_trajGen.setSpeedFactor(1);
@@ -138,12 +147,18 @@ classdef Robot
                 obj.arm2R_cmd.velocity(2) = vel(2);
                 obj.arm2R_group.send(obj.arm2R_cmd);
             end
-            
-
-            obj.arm_target_reached = true;
+            %}
+            obj.arm_target_reached = 1;
         end
 
-        function gripper_open(obj)
+        function hinge_move(obj, hinge_target) % TODO: Copy code from arm2R_move, adjust it to just one motor
+            disp("Moving arm hinge!");
+            obj.arm_target_reached = 1;
+        end
+
+        function gripper_open(obj) % Works
+            disp("Opening Gripper");
+            %{
             fbk = obj.group.getNextFeedback();
             gripper_angle = fbk.position(4);
             while gripper_angle > obj.gripper_open_angle 
@@ -155,12 +170,15 @@ classdef Robot
             end
             obj.cmd.velocity = [0 0 0 0];
             obj.group.send(obj.cmd);
+            %}
             obj.gripper_opened = true;
             %disp("Gripper opened!");
         end
 
 
-        function gripper_close(obj)
+        function gripper_close(obj) % Works
+            disp("Closing Gripper");
+            %{
             fbk = obj.group.getNextFeedback();
             gripper_angle = fbk.position(4);
             while gripper_angle < obj.gripper_closed_angle 
@@ -181,13 +199,15 @@ classdef Robot
                 obj.figure_gripped = false;
                 disp("Figure not gripped!");
             end
+            %}
             obj.gripper_opened = false;
         end
 
 
-        function arm_stop()
-            obj.cmd.velocity = [0 0 0 0];
-            obj.group.send(obj.cmd);
+        function arm_stop(obj)
+            disp("Stopping Arm");
+            %obj.cmd.velocity = [0 0 0 0];
+            %obj.group.send(obj.cmd);
         end
     end
 
